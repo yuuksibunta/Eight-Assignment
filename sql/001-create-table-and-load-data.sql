becbf2bef05a0e946a00bb4d6e7fbeb067a9c53f
yuuksibunta <idayuuki.tiu@gmail.com>
2023-12-08 12:29:18 +0900

    first commit

---
+++ sql/001-create-table-and-load-data.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS names;
+CREATE TABLE names (
+ id int unsigned AUTO_INCREMENT,
+ name VARCHAR(20) NOT NULL,
+ PRIMARY KEY(id)
+);
+INSERT INTO names (name) VALUES ('name1');
+INSERT INTO names (name) VALUES ('name2');
+INSERT INTO names (name) VALUES ('yuki');
---
+++ src/main/java/com/example/name/NameMapper.java
@@ -0,0 +1,16 @@
+package com.example.name;
+
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface NameMapper {
+    @Select("SELECT * FROM names")
+    List<Name> findAll();
+
+    @Select("SELECT * FROM names WHERE name LIKE CONCAT(#{prefix}, '%')")
+    List<Name> findByNameStartingWith(String prefix);
+}
