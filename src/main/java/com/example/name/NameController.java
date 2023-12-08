a1a453c4404ac6e810a02eae8567f4e7d7daa770
yuuksibunta <idayuuki.tiu@gmail.com>
2023-12-08 15:56:45 +0900

    name

--- src/main/java/com/example/name/NameController.java
+++ src/main/java/com/example/name/NameController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-
 @RestController
 public class NameController {
 
@@ -17,10 +16,13 @@ public class NameController {
     }
 
     @GetMapping("/names")
-    public List<Name> findByNames(@RequestParam String startsWith) {
-        return nameMapper.findByNameStartingWith(startsWith);
+    public List<Name> findByNames(@RequestParam(required = false) String startsWith) {
+        if (startsWith == null) {
+            // startsWith パラメータが提供されない場合のデフォルト値やオプションの処理を実装
+            // この例では null を指定した場合、全ての名前を取得するなどの処理を行うことが考えられます。
+            return nameMapper.findAllNames();
+        } else {
+            return nameMapper.findByNameStartingWith(startsWith);
         }
-
+    }
 }
-
-
